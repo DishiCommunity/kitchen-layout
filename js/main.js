9dcd570676c2e54f0c6d3063e5a25517e3aaeb65
BillChabzz <chaby9.8@gmail.com>
2016-03-30 15:27:44 +0300

    fixed form appearances

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
 var dishLink = document.getElementsByClassName("addDish");
 var recipeLink = document.getElementsByClassName("addRecipe");
+
+ $('.addDish').on('click', function(){
+          if($('.form-addDish').hasClass("is-animated-in")) {
+              $.addClass("is-animated-in");
+      });
+         $('.close-button').on('click', function(){
+             $('.add').removeClass("is-animated-out").addClass("is-animated-out");
+         });
+     
+$('.addDish, .addRecipe, .addPost .close-button').on('click' , function(){
+    if($(this).hasClass("is-animated-in")){
+        $(this).toggle("is-animated-out");
+    }
+});     
