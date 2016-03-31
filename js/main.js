01cb6836dbbc9ec82d47288f6152796aea8914e1
BillChabzz <chaby9.8@gmail.com>
2016-03-31 14:01:22 +0300

    fixed forms

--- js/main.js
+++ js/main.js
@@ -1,16 +1,46 @@
-var dishLink = document.getElementsByClassName("addDish");
-var recipeLink = document.getElementsByClassName("addRecipe");
+$(function () {
+    $('.form-addRecipe').hide();
+    $('.form-addPost').hide();
+    $('.form-addDish').hide();
+    $('.addDish').click(function () {
+        $('.page-overlay').show();
+        $(".form-addItem-holder").show();
+        $('.form-addDish').slideToggle(500);
+        return false;
+    });
+    $('.addRecipe').click(function () {
+         $('.page-overlay').show();
+        $(".form-addItem-holder").show();
+        $('.form-addRecipe').slideToggle(500);
+    });
+    $('.addPost').click(function () {
+         $('.page-overlay').show();
+        $(".form-addItem-holder").show();
+        $('.form-addPost').slideToggle(500);
+        return false;
+    });
+    $('#close-dishForm').find('.fa-times').click(function () {
+        $('#close-dishForm').animate({
+            height: "0",
+            display: "none"
+        },500).hide();
+        $('.page-overlay').css("display","none");
 
- $('.addDish').on('click', function(){
-          if($('.form-addDish').hasClass("is-animated-in")) {
-              $.addClass("is-animated-in");
-      });
-         $('.close-button').on('click', function(){
-             $('.add').removeClass("is-animated-out").addClass("is-animated-out");
-         });
-     
-$('.addDish, .addRecipe, .addPost .close-button').on('click' , function(){
-    if($(this).hasClass("is-animated-in")){
-        $(this).toggle("is-animated-out");
-    }
-});     
+    });
+    $('#close-recipeForm').find('.fa-times').click(function () {
+        $('#close-recipeForm').animate({
+            height: "0",
+            display: "none"
+        },500).hide();
+        $('.page-overlay').css("display","none");
+
+    });
+    $('#close-postForm').find('.fa-times').click(function () {
+        $('#close-postForm').animate({
+            height: "0",
+            display: "none"
+        },500).hide();
+        $('.page-overlay').css("display","none");
+
+    });
+});
